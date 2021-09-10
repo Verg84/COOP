b80028c6fcbdabc97ca3acfb1384d688b75f09a1
Verg84 <50481854+Verg84@users.noreply.github.com>
2021-09-10 20:51:49 +0200

    exercise01 and exercise02

---
+++ CH01/exercise01.cpp
@@ -0,0 +1,11 @@
+// A program that outputs a text on the screen
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<endl
+        <<"Oh what"<<endl<<"a happy day!"<<"Oh yes,"<<endl
+        <<"what a happy day";
+        
+        return 0;
+}
